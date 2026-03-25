aad8be2005dead6712ed11ce3f4fa8d575fa2f42
glumm <jlminot13@gmail.com>
2026-03-25 11:55:55 -0400

    creating classes

--- BLPVisualizer/visual.py
+++ BLPVisualizer/visual.py
@@ -1,17 +1,39 @@
-#objects 
-class subjects: 
+#global array storing subjects
+subjects = []
+
+#global array storing objects 
+objects = []
+
+
+class subjects:
+    #initializes new subject
+    def __init__(self, name, maximum, starting):
+        self.name = name    
+        self.maximum = maximum  
+        self.starting = starting
+
+    #Adds a new subject to the system. A subject cannot be added if 
+    #their starting level is higher than their maximum clearance.
+    def add_subject(subj):
+        global subjects
+        if (self.starting <= self.naximum)
+            subjects.append(subj)
+
 
 
 class objects: 
+    #initializes new object
+    def __init__(self, filename, level)
+        self.filename = filename
+        self.level = level
+
+    #Adds a new object to the system with a designated security
+    #classification.
+    def add_object(obj)
+        global objects
+        subjects.append(obj)
 
 
-#Adds a new subject to the system. A subject cannot be added if 
-#their starting level is higher than their maximum clearance.
-def add_subject()
-
-#Adds a new object to the system with a designated security
-#classification.
-def add_object()
 
 #Checks if a subject's current security level exactly matches an
 #object's security level. Returns true if they match, and false otherwise.
